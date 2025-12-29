c4ffc9ea5713bfb7b7029691db5a9ff552a42708
Sourodyuti Biswas Sanyal <sourodyuti.biswas.sanyal.14@gmail.com>
2025-12-30 04:09:50 +0530

    Initialize models module

---
+++ src/models/__init__.py
@@ -0,0 +1,11 @@
+"""
+Model Definitions for Knowledge Distillation
+
+Supports:
+- CNN models (ResNet family)
+- Vision Transformers (ViT family)
+
+All models are compatible with 224x224 RGB images.
+"""
+
+__version__ = "1.0.0"
